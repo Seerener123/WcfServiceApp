86af17bd931b8572bc6929162a8991a23bc25843
zeroJava <zero_2009@hotmail.co.uk>
2018-08-11 22:42:11 +0100

    folder refactor
    Refactored the wcfservceapp folder structure, by removing the base-operation-service folder, and moving the content to the service-interface folder inside the messaging folder. Also made changes to the namespace of all the interfaces that were moved.

---
+++ WcfServiceApp/Messaging/ServiceInterfaces/IRetrieveUserService.cs
@@ -0,0 +1,18 @@
+﻿using MessageDbLib.MessagingEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceApp.Messaging.ServiceInterfaces
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRetrieveUserService" in both code and config file together.
+    [ServiceContract]
+    public interface IRetrieveUserService
+    {
+        [OperationContract]
+        List<UserTable> GetAllUsers();
+    }
+}
---
+++ WcfServiceApp/Messaging/Services/RetrieveUserService.svc.cs
@@ -0,0 +1,46 @@
+﻿using MessageDbLib.DbContextFactorys;
+using MessageDbLib.DbRetrievals;
+using MessageDbLib.MessagingEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfServiceApp.Exceptions.Datacontacts;
+using WcfServiceApp.Messaging.ServiceInterfaces;
+
+namespace WcfServiceApp.Messaging.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RetrieveUserService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RetrieveUserService.svc or RetrieveUserService.svc.cs at the Solution Explorer and start debugging.
+    public class RetrieveUserService : IRetrieveUserService
+    {
+        public List<UserTable> GetAllUsers()
+        {
+            try
+            {
+                RetrieveUserClass retrieveUser = new RetrieveUserClass(DatabaseOptionConfigRetriever.DatabaseOptionAppSetting);
+                IList<UserTable> user = retrieveUser.GetAllEntities();
+                return user.ToList();
+            }
+            catch (Exception exception)
+            {
+                var error = new EntityErrorContract
+                {
+                    Message = exception.Message
+                };
+                throw new FaultException<EntityErrorContract>(error);
+            }
+        }
+
+        private void ThrowErrorMessage(string message)
+        {
+            var error = new EntityErrorContract
+            {
+                Message = message
+            };
+            throw new FaultException<EntityErrorContract>(error);
+        }
+    }
+}
