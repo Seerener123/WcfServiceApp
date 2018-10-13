3fa7d0a22c21dd26e917afecccc80080ecfc6895
zeroJava <zero_2009@hotmail.co.uk>
2018-10-13 18:07:44 +0100

    progress on message queueing

---
+++ MessageMqLib/MqProducerClasses/RabbitMqProducerClass.cs
@@ -0,0 +1,38 @@
+﻿using MessageMqLib.MqInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace MessageMqLib.MqProducerClasses
+{
+    public class RabbitMqProdcuerClass : IMessageQueueProducer
+    {
+        public void ExecuteMessageQueueing<TMessage>(TMessage message)
+        {
+            ConnectionFactory connectionFactory = CreateConnectionFactory();
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                ExecuteMessageSending(connection, message);
+            }
+        }
+
+        private ConnectionFactory CreateConnectionFactory()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = "";
+            return connectionFactory;
+        }
+
+        private void ExecuteMessageSending<TMessage>(IConnection connection, TMessage message)
+        {
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                // send message part here
+            }
+        }
+    }
+}
